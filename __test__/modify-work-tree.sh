efbabfb0ae927292a915695b2f9f4ac03ccec094
Srinivas Bommadevara <srinivas.bommadevara@hitachivantara.com>
2021-05-20 23:54:20 +0530

    fixing tests #7

--- __test__/modify-work-tree.sh
+++ __test__/modify-work-tree.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -f "./basic/basic-file.txt" ]; then
+if [ ! -f "./checkout/basic/basic-file.txt" ]; then
     echo "Expected basic file does not exist"
     exit 1
 fi
 
-echo hello >> ./basic/basic-file.txt
-echo hello >> ./basic/new-file.txt
-git -C ./basic status
+echo hello >> ./checkout/basic/basic-file.txt
+echo hello >> ./checkout/basic/new-file.txt
+git -C ./checkout/basic status
