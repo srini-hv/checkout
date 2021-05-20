4de664e40cc64f8728756d893aa756289eb19fd9
Srinivas Bommadevara <srinivas.bommadevara@hitachivantara.com>
2021-05-20 23:48:25 +0530

    fixing tests #6

--- __test__/verify-lfs.sh
+++ __test__/verify-lfs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -f "./lfs/regular-file.txt" ]; then
+if [ ! -f "./checkout/lfs/regular-file.txt" ]; then
     echo "Expected regular file does not exist"
     exit 1
 fi
 
-if [ ! -f "./lfs/lfs-file.bin" ]; then
+if [ ! -f "./checkout/lfs/lfs-file.bin" ]; then
     echo "Expected lfs file does not exist"
     exit 1
 fi
--- __test__/verify-submodules-recursive.sh
+++ __test__/verify-submodules-recursive.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [ ! -f "./submodules-recursive/regular-file.txt" ]; then
+if [ ! -f "./checkout/submodules-recursive/regular-file.txt" ]; then
     echo "Expected regular file does not exist"
     exit 1
 fi
 
-if [ ! -f "./submodules-recursive/submodule-level-1/submodule-file.txt" ]; then
+if [ ! -f "./checkout/submodules-recursive/submodule-level-1/submodule-file.txt" ]; then
     echo "Expected submodule file does not exist"
     exit 1
 fi
 
-if [ ! -f "./submodules-recursive/submodule-level-1/submodule-level-2/nested-submodule-file.txt" ]; then
+if [ ! -f "./checkout/submodules-recursive/submodule-level-1/submodule-level-2/nested-submodule-file.txt" ]; then
     echo "Expected nested submodule file does not exists"
     exit 1
 fi
 
 echo "Testing persisted credential"
-pushd ./submodules-recursive/submodule-level-1/submodule-level-2
+pushd ./checkout/submodules-recursive/submodule-level-1/submodule-level-2
 git config --local --name-only --get-regexp http.+extraheader && git fetch
 if [ "$?" != "0" ]; then
     echo "Failed to validate persisted credential"
